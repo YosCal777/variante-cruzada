d61d5a098416d002e6eee6a5f06aac7a91f391e5
YosCal777 <143665213+YosCal777@users.noreply.github.com>
2023-09-30 00:23:57 -0400

    Add files via upload

---
+++ send-results.js
@@ -0,0 +1,12 @@
+//Exportando consulta API
+export async function result() {
+    try {
+      const response = await fetch("https://result-roulett.onrender.com");
+      const data = await response.json();
+      return data; // Devuelve los datos
+    } catch (err) {
+      console.log('Fetch Error :-S', err);
+    }
+
+ }
+ 
